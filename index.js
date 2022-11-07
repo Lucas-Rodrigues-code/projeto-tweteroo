079972bdaf09c6b369e9a27ad3ad0c8d9e7d74a0
Lucas <Rodriguesdesouzal28@gmail.com>
2022-11-07 10:00:12 -0300

    Requisito post/tweets

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.use(cors());
 app.use(json());
 
 const users = []
+const tweets = []
 
+let user = {}
 
 app.post('/sign-up', (req, res) => {
     user = {
@@ -16,3 +18,28 @@ app.post('/sign-up', (req, res) => {
     users.push(user)
     res.send("ok");
   })
+
+  app.post('/tweets', (req, res) => {
+
+    const obj = {
+      username: req.body.username,
+      tweet: req.body.tweet
+    }
+  
+    const avatar = verificaIguais(user.username, obj.username)
+  
+    const tweet = {
+      username: obj.username,
+      avatar: avatar,
+      tweet: obj.tweet
+  
+    }
+  
+    tweets.push(tweet)
+    res.send("ok");
+  })
+
+  app.listen(5000, () => {
+    console.log("Rodando em http://localhost:5000")
+  });
+  
