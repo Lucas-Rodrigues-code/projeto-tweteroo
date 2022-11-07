0e246cb1300b75d7da573575f15f4761febc44e5
Lucas <Rodriguesdesouzal28@gmail.com>
2022-11-07 10:02:51 -0300

    Requisito get/tweets

--- index.js
+++ index.js
@@ -12,34 +12,47 @@ let user = {}
 
 app.post('/sign-up', (req, res) => {
     user = {
-      username: req.body.username,
-      avatar: req.body.avatar
+        username: req.body.username,
+        avatar: req.body.avatar
     }
     users.push(user)
     res.send("ok");
-  })
+})
 
-  app.post('/tweets', (req, res) => {
+app.post('/tweets', (req, res) => {
 
     const obj = {
-      username: req.body.username,
-      tweet: req.body.tweet
+        username: req.body.username,
+        tweet: req.body.tweet
     }
-  
+
     const avatar = verificaIguais(user.username, obj.username)
-  
+
     const tweet = {
-      username: obj.username,
-      avatar: avatar,
-      tweet: obj.tweet
-  
+        username: obj.username,
+        avatar: avatar,
+        tweet: obj.tweet
+
     }
-  
+
     tweets.push(tweet)
     res.send("ok");
-  })
+})
 
-  app.listen(5000, () => {
+app.get('/tweets', (req, res) => {
+
+    res.send(tweets);
+});
+
+app.listen(5000, () => {
     console.log("Rodando em http://localhost:5000")
-  });
-  
+});
+
+function verificaIguais(name, nametweet) {
+    if (nametweet === name) {
+        return user.avatar
+    } else {
+        return null
+    }
+
+}
